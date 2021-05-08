84982b362b997241278b6033230ced4f0fdc4812
bushig <azerot966@gmail.com>
2021-05-08 14:17:26 +0300

    delete server method

--- backend/webbit/api/routers/rabbit/server.py
+++ backend/webbit/api/routers/rabbit/server.py
@@ -4,8 +4,10 @@ import json
 from typing import List
 
 import aioredis
-from fastapi import APIRouter, BackgroundTasks, HTTPException
+from fastapi import APIRouter, BackgroundTasks, HTTPException, Response
 from datetime import datetime, timedelta, timezone
+
+from starlette.status import HTTP_204_NO_CONTENT
 
 from webbit.core.schemas import RabbitData, QueueMeta
 
@@ -19,12 +21,16 @@ async def create_server(server_info: RabbitServerCreateSchema):
     result = await RabbitServer.create(**server_info.dict(exclude_unset=True))
     return {"id": result.id}
 
+
 @router.get("/", response_model=List[RabbitServerSchema])
 async def get_servers_list():
     results = RabbitServer.all()
     return await RabbitServerSchema.from_queryset(results)
+
+
 #
-# @router.get("/", response_model=List[RabbitServerSchema])
-# async def get_servers_list():
-#     results = await RabbitServer.get()
-#     return await RabbitServerSchema.from_tortoise_orm(results)
+@router.delete("/{server_id}", response_model=None, status_code=HTTP_204_NO_CONTENT)
+async def delete_server(server_id: int):
+    server = await RabbitServer.get(id=server_id)
+    await server.delete()
+    return Response(status_code=HTTP_204_NO_CONTENT)
